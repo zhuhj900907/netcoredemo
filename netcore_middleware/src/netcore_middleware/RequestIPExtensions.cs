842345e54fc77dc8678577615b0b14a49453562f
zhuhjtcx@qq.com <zzbj891016>
2017-05-22 17:36:04 +0800

    df

---
+++ netcore_middleware/src/netcore_middleware/RequestIPExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace netcore_middleware
+{
+    public static class RequestIPExtensions
+    {
+        public static IApplicationBuilder UseRequestIP(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestIPMiddleware>();
+        }
+    }
+}
---
+++ netcore_middleware/src/netcore_middleware/RequestIPMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace netcore_middleware
+{
+    /// <summary>
+    /// ip地址的中间件
+    /// </summary>
+    public class RequestIPMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="next"></param>
+        /// <param name="loggerFactory"></param>
+        public RequestIPMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<RequestIPMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation("User IP: " + context.Connection.RemoteIpAddress.ToString());
+            await _next.Invoke(context);
+        }
+    }
+}
